c85ef173f41c6cb1a2aed06ce0216dbe05ca1cb5
Paul Zijlmans <paulzijlmans@hotmail.com>
2021-06-16 16:02:49 +0200

    first commit

---
+++ src/main/java/nl/paulzijlmans/springboot/basics/springbootin10steps/BooksController.java
@@ -0,0 +1,15 @@
+package nl.paulzijlmans.springboot.basics.springbootin10steps;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+public class BooksController {
+    @GetMapping("/books")
+    public List<Book> getAllBooks() {
+        return Collections.singletonList(new Book(1L, "Mastering Spring 5.0", "Ranga Karanam"));
+    }
+}
